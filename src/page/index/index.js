4611878702c8b40aeb338be95f8cb2774eb39ae3
ice-berg123 <79825651+ice-berg123@users.noreply.github.com>
2022-06-05 14:26:29 +0800

    添加排行榜滑动边界

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -4,23 +4,23 @@ const pervw = document.body.clientWidth / 100
 const pervh = document.body.clientHeight / 100
 const navSort = document.querySelectorAll(".nav_sort")
 navSort.forEach((item) => {
-    item.addEventListener("click",() => {
-        navSort.forEach((item,index)=>{
+    item.addEventListener("click", () => {
+        navSort.forEach((item, index) => {
             item.style.color = "rgb(168,168,168)"
             item.lastElementChild.style.visibility = "hidden"
         })
         item.style.color = "rgb(34,213,156)"
         item.lastElementChild.style.visibility = "visible"
-        
+
     })
 })
 const recommendPage = document.querySelector(".recommend_page")
 const chartPage = document.querySelector(".chart_page")
-navSort[0].addEventListener("click",()=>{
+navSort[0].addEventListener("click", () => {
     recommendPage.style.display = "block"
     chartPage.style.display = "none"
 })
-navSort[1].addEventListener("click",()=>{
+navSort[1].addEventListener("click", () => {
     recommendPage.style.display = "none"
     chartPage.style.display = "block"
 })
@@ -39,35 +39,35 @@ const searchList = document.querySelector(".search_list")
 const delete1 = document.querySelector(".delete1")
 const delete2 = document.querySelector(".delete2")
 const chartList = document.querySelectorAll(".chart_list")
-function ThingsUnsearch(){
+function ThingsUnsearch() {
     unsearch.style.display = "none"
     onsearch.style.display = "block"
     searchwait.style.display = "none"
     searching.style.display = "block"
     searchPage[1].style.display = "block"
     recommendSeet.style.display = "none"
-    if(historyList.children.length != 0){
+    if (historyList.children.length != 0) {
         searchPage[0].style.display = "block"
     }
 }
-function ThingsSearchWait(){
-    if(InputSearch.value == ""){
+function ThingsSearchWait() {
+    if (InputSearch.value == "") {
         delete1.style.display = "none"
-    }else{
+    } else {
         delete1.style.display = "block"
     }
     searchwait.style.display = "none"
     searching.style.display = "block"
 }
-InputSearch.addEventListener('keypress', function(e) {
+InputSearch.addEventListener('keypress', function (e) {
     if (e.keyCode === 13) {
         searchDate(InputSearch.value)
     }
 })
-unsearch.addEventListener("click",()=>{
+unsearch.addEventListener("click", () => {
     ThingsUnsearch()
 })
-cancleSearch.addEventListener("click",()=>{
+cancleSearch.addEventListener("click", () => {
     unsearch.style.display = "flex"
     onsearch.style.display = "none"
     searchwait.style.display = "none"
@@ -78,46 +78,46 @@ cancleSearch.addEventListener("click",()=>{
     searchList.style.display = "none"
     InputSearch.value = ""
 })
-searchwait.addEventListener("click",()=> {
+searchwait.addEventListener("click", () => {
     ThingsSearchWait()
 })
-searching.children[1].addEventListener("blur",()=>{
-    if(searching.children[1].value == ""){
+searching.children[1].addEventListener("blur", () => {
+    if (searching.children[1].value == "") {
         searchwait.style.display = "flex"
         searching.style.display = "none"
     }
 })
-delete1.addEventListener("click",() => {
+delete1.addEventListener("click", () => {
     InputSearch.value = ""
     searchList.style.display = "none"
     searchPage[1].style.display = "block"
     searchPage[0].style.display = "block"
 })
-InputSearch.addEventListener("input",() => {
-    if(InputSearch.value == ""){
+InputSearch.addEventListener("input", () => {
+    if (InputSearch.value == "") {
         delete1.style.display = "none"
-    }else{
+    } else {
         delete1.style.display = "block"
     }
 })
-delete2.addEventListener("click",() => {
+delete2.addEventListener("click", () => {
     historyList.innerHTML = ""
     searchPage[0].style.display = "none"
 })
 
 
-async function DateInit(){
+async function DateInit() {
     hotSearchInit()
     chartPageInit()
     sheetDateInit()
 }
-function checkNaN(item){
-    if(item == ""){
+function checkNaN(item) {
+    if (item == "") {
         item = "0px"
     }
     return item
 }
-async function hotSearchInit(){
+async function hotSearchInit() {
     const hotList = document.querySelector(".hot_list")
     const hotSearchItemArrs = await GetResource("/hot")
     hotSearchItemArrs.forEach((item) => {
@@ -125,18 +125,18 @@ async function hotSearchInit(){
         tempItem.className = "hot_item"
         tempItem.textContent = item
         hotList.appendChild(tempItem)
-        tempItem.addEventListener("click",() => {
+        tempItem.addEventListener("click", () => {
             InputSearch.value = tempItem.textContent
             ThingsSearchWait()
             searchDate(tempItem.textContent)
         })
     })
-} 
-async function chartPageInit(){
+}
+async function chartPageInit() {
     const chartPage = document.querySelector(".chart_page")
     const chartPageData = await GetResource("/ranking")
 
-    chartPageData.forEach((item,index) => {
+    chartPageData.forEach((item, index) => {
         const tempChartList = document.createElement("div")
         tempChartList.className = "chart_list"
         tempChartList.innerHTML = "<div class='chart_list_left'><div class='chart_title'></div><div class='chart_item_box'><div class='chart_item'><div class='chart_item_index'>1.</div><div class='song_name'></div><div class='song_author'></div></div></div><div class='chart_item_box'><div class='chart_item'><div class='chart_item_index'>2.</div><div class='song_name'></div><div class='song_author'></div></div></div><div class='chart_item_box'><div class='chart_item'><div class='chart_item_index'>3.</div><div class='song_name'></div><div class='song_author'></div></div></div></div><div class='chart_list_right'><img class='chart_img' src='../../picture/song.jpg' alt=''><div class='week_update'></div><div class='numbers'></div></div>"
@@ -147,68 +147,68 @@ async function chartPageInit(){
         let startPosY
         let startTop
         let flag = 0
-        tempChartList.addEventListener("touchstart",(e) => {
+        tempChartList.addEventListener("touchstart", (e) => {
             tempChartList.style.backgroundColor = "rgb(255,255,255)"
             startPosY = e.touches[0].pageY
             startTop = tempChartList.style.top
             let points = 255
             startTime = new Date()
-            choiceColorInterval = setInterval(()=>{
+            choiceColorInterval = setInterval(() => {
                 points -= 2
                 tempChartList.style.backgroundColor = `rgb(${points},${points},${points})`
-            },50);
-            setTimeout(()=> {
+            }, 50);
+            setTimeout(() => {
                 clearInterval(choiceColorInterval)
-            },500)
+            }, 500)
             startTime = new Date()
         })
-        tempChartList.addEventListener("touchmove",(e) => {
+        tempChartList.addEventListener("touchmove", (e) => {
             clearInterval(choiceColorInterval)
             let tempTime = new Date()
-            if(tempTime-startTime > 500){
+            if (tempTime - startTime > 500) {
                 tempChartList.style.zIndex = 20
-                let subPos = e.touches[0].pageY-startPosY
+                let subPos = e.touches[0].pageY - startPosY
                 tempChartList.style.top = checkNaN(tempChartList.style.top)
-                console.log(tempChartList.offsetTop,tempChartList.index,index,parseInt(tempChartList.style.top),tempChartList.style.top,15*pervw)
-                if( !((tempChartList.index == 0 && subPos < 0) || (tempChartList.index == chartPage.children.length-1 && subPos > 0)))
-                {
-                    tempChartList.style.top = parseInt(tempChartList.style.top)+subPos + "px"
+                if (!((tempChartList.offsetTop < 0) || (tempChartList.offsetTop + tempChartList.offsetHeight > chartPage.offsetHeight))) {
+                    tempChartList.style.top = parseInt(tempChartList.style.top) + subPos + "px"
+                } else if (tempChartList.offsetTop <= 0) {
+                    tempChartList.style.top = parseInt(tempChartList.style.top) + 1 + "px"
+                } else if (tempChartList.offsetTop + tempChartList.offsetHeight >= chartPage.offsetHeight) {
+                    tempChartList.style.top = parseInt(tempChartList.style.top) - 1 + "px"
                 }
-            
-                startPosY = e.touches[0].pageY
-                if((parseInt(tempChartList.style.top) > 15*pervw || parseInt(tempChartList.style.top) < -15*pervw) && flag == 0){
 
-                    if(parseInt(tempChartList.style.top) > 0){
-                        
-                        chartPage.children[tempChartList.index+1].style.top = - 34.9*pervw + "px" 
-                        chartPage.children[tempChartList.index+1].index--
-                    }else{
-                        
-                        chartPage.children[tempChartList.index-1].style.top = 34.9*pervw + "px" 
-                        chartPage.children[tempChartList.index-1].index++
+                startPosY = e.touches[0].pageY
+                if ((parseInt(tempChartList.style.top) > 15 * pervw || parseInt(tempChartList.style.top) < -15 * pervw) && flag == 0) {
+                    if (parseInt(tempChartList.style.top) >= 0) {
+
+                        chartPage.children[tempChartList.index + 1].style.top = - 34.9 * pervw + "px"
+                        chartPage.children[tempChartList.index + 1].index--
+                    } else {
+
+                        chartPage.children[tempChartList.index - 1].style.top = 34.9 * pervw + "px"
+                        chartPage.children[tempChartList.index - 1].index++
                     }
                     flag = 1;
                 }
                 e.preventDefault()
-            }else{
+            } else {
                 startTime = tempTime
             }
-
         })
-        tempChartList.addEventListener("touchend",(e) => {
+        tempChartList.addEventListener("touchend", (e) => {
             clearInterval(choiceColorInterval)
             flag = 0
-            if(parseInt(tempChartList.style.top) > 15*pervw || parseInt(tempChartList.style.top) < -15*pervw){
+            if (parseInt(tempChartList.style.top) > 15 * pervw || parseInt(tempChartList.style.top) < -15 * pervw) {
                 console.log(parseInt(tempChartList.style.top))
                 let temp = chartPage.removeChild(chartPage.children[tempChartList.index])
-                if(parseInt(tempChartList.style.top) > 0){
-                    chartPage.insertBefore(temp,chartPage.children[tempChartList.index+1])
-                    chartPage.children[tempChartList.index+1].style.top = ""
+                if (parseInt(tempChartList.style.top) > 0) {
+                    chartPage.insertBefore(temp, chartPage.children[tempChartList.index + 1])
+                    chartPage.children[tempChartList.index + 1].style.top = ""
                     chartPage.children[tempChartList.index].style.top = ""
                     tempChartList.index++
-                }else{
-                    chartPage.insertBefore(temp,chartPage.children[tempChartList.index-1])
-                    chartPage.children[tempChartList.index-1].style.top = ""
+                } else {
+                    chartPage.insertBefore(temp, chartPage.children[tempChartList.index - 1])
+                    chartPage.children[tempChartList.index - 1].style.top = ""
                     chartPage.children[tempChartList.index].style.top = ""
                     tempChartList.index--
                 }
@@ -218,8 +218,8 @@ async function chartPageInit(){
             tempChartList.style.backgroundColor = "rgb(255,255,255)"
         })
     })
-    chartPageData.forEach((item,index) => {
-        
+    chartPageData.forEach((item, index) => {
+
         const chartTitleArr = document.querySelectorAll(".chart_title")
         const songName = document.querySelectorAll(".song_name")
         const songAuthor = document.querySelectorAll(".song_author")
@@ -229,44 +229,44 @@ async function chartPageInit(){
         chartImg[index].src = item.cover
         chartTitleArr[index].textContent = item.title
         weekUpdate[index].textContent = `每${item.update_frequence}更新`
-        numbers[index].textContent = (item.views/10000).toFixed(1) + "万"
-        for(let i = 0; i < 3; i++){
-            songName[index*3+i].textContent = item.top3[i].title+"-"
+        numbers[index].textContent = (item.views / 10000).toFixed(1) + "万"
+        for (let i = 0; i < 3; i++) {
+            songName[index * 3 + i].textContent = item.top3[i].title + "-"
             let actorString = ""
-            item.top3[i].artist.forEach((name,index2) => {
-                if(index2 == 0){
+            item.top3[i].artist.forEach((name, index2) => {
+                if (index2 == 0) {
                     actorString += name
-                }else{
+                } else {
                     actorString += ` / ${name}`
                 }
             })
-            songAuthor[index*3+i].textContent = actorString
+            songAuthor[index * 3 + i].textContent = actorString
         }
-        
-    }) 
+
+    })
 }
-async function sheetDateInit(){
+async function sheetDateInit() {
     const sheetDateArr = await GetResource("/recommendations")
     const sheetBox = document.querySelectorAll(".sheet_box")
-    sheetDateArr.offical.forEach((item,index) => {
+    sheetDateArr.offical.forEach((item, index) => {
         const tempSheetItemBox = document.createElement("div")
         tempSheetItemBox.className = "sheet_item"
         tempSheetItemBox.innerHTML = "<img class='sheet_item_img' src='../../picture/song.jpg' alt=''><div class='sheet_numbers'>555万</div><div class='sheet_tiem_title'>大大大</div>"
         tempSheetItemBox.children[0].src = sheetDateArr.offical[index].cover
-        tempSheetItemBox.children[1].textContent = (sheetDateArr.offical[index].views/10000).toFixed(1) + "万"
+        tempSheetItemBox.children[1].textContent = (sheetDateArr.offical[index].views / 10000).toFixed(1) + "万"
         tempSheetItemBox.children[2].textContent = sheetDateArr.offical[index].title
         sheetBox[0].appendChild(tempSheetItemBox)
     })
-    sheetDateArr.tatsujin.forEach((item,index) => {
+    sheetDateArr.tatsujin.forEach((item, index) => {
         const tempSheetItemBox = document.createElement("div")
         tempSheetItemBox.className = "sheet_item"
         tempSheetItemBox.innerHTML = "<img class='sheet_item_img' src='../../picture/song.jpg' alt=''><div class='sheet_numbers'>555万</div><div class='sheet_tiem_title'>大大大</div>"
         tempSheetItemBox.children[0].src = sheetDateArr.tatsujin[index].cover
-        tempSheetItemBox.children[1].textContent = (sheetDateArr.tatsujin[index].views/10000).toFixed(1) + "万"
+        tempSheetItemBox.children[1].textContent = (sheetDateArr.tatsujin[index].views / 10000).toFixed(1) + "万"
         tempSheetItemBox.children[2].textContent = sheetDateArr.tatsujin[index].title
         sheetBox[1].appendChild(tempSheetItemBox)
     })
-    sheetDateArr.column.forEach((item,index) => {
+    sheetDateArr.column.forEach((item, index) => {
         const tempSheetItemBox = document.createElement("div")
         tempSheetItemBox.className = "sheet_item"
         tempSheetItemBox.innerHTML = "<div class='sheet_big_box'><div class='sheet_mid_box'><div class='sheet_small_picture'></div><div class='column_name'></div></div><div class='sheet_decoration'></div></div>"
@@ -277,28 +277,27 @@ async function sheetDateInit(){
         sheetBox[2].appendChild(tempSheetItemBox)
     })
 }
-async function searchDate(value){
+async function searchDate(value) {
     let flag = 0
-    if(historyList.children.length != 0){
-        for(let i = 0; i < historyList.children.length;i++){
-            console.log(value == historyList.children[i].textContent)
-            if(value == historyList.children[i].textContent){
+    if (historyList.children.length != 0) {
+        for (let i = 0; i < historyList.children.length; i++) {
+            if (value == historyList.children[i].textContent) {
                 flag = 1
             }
         }
     }
-    if(flag != 1){
+    if (flag != 1) {
         searchList.innerHTML = ""
         const tempHistoryItem = document.createElement("div")
         tempHistoryItem.className = "history_item"
         tempHistoryItem.textContent = value
         historyList.appendChild(tempHistoryItem)
-        tempHistoryItem.addEventListener("click",()=> {
-        InputSearch.value = tempHistoryItem.textContent
-        searchDate(tempHistoryItem.textContent)
-    })
+        tempHistoryItem.addEventListener("click", () => {
+            InputSearch.value = tempHistoryItem.textContent
+            searchDate(tempHistoryItem.textContent)
+        })
     }
-    if(historyList.children.length != 0){
+    if (historyList.children.length != 0) {
         searchPage[0].style.display = "block"
     }
     const sheetDateArr = await GetResource(`/search?keyword=${value}`)
@@ -310,11 +309,11 @@ async function searchDate(value){
         searchList.appendChild(tempSearchList)
         tempSearchList.children[0].textContent = item.title
         let author = ""
-        item.artist.forEach((item,index)=>{
-            if(index == 0){
+        item.artist.forEach((item, index) => {
+            if (index == 0) {
 
                 author += item
-            }else{
+            } else {
                 author += " · " + item
             }
         })
